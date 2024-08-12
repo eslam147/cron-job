5da31e96f658310617a0b5394de891c1c52a2ee6
eslam147 <emomemo4555@gmail.com>
2024-08-12 10:03:09 +0300

    Resolve merge conflicts

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,9 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('notify:lessons')->hourly();
-        $schedule->command('queue:work')->everyMinute();
-        $schedule->command('queue:restart')->everyFiveMinutes();
+        // $schedule->command('inspire')->hourly();
     }
 
     /**
